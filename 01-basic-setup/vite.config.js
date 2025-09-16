799bb02322fd6c9bbace84109493cc3812f08e89
winverse <public.winverse@gmail.com>
2025-09-16 16:44:44 +0900

    .gitignore 및 Vite 구성 수정, 자동 생성된 routeTree 파일 삭제

--- 01-basic-setup/vite.config.js
+++ 01-basic-setup/vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ disableTypes: true })],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
